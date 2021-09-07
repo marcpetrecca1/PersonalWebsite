61f149a09dc19aaed28664c4f144bc4351c9f01f
MarcAnthony Petrecca <marcanthony.petrecca@gmail.com>
2021-09-07 15:53:53 -0400

    Small change to presets in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
       },
       {
         test: /\.css$/,
