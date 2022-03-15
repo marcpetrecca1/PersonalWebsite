dba059f5b1ad1bdc0ba1724e0f462888055397e5
MarcAnthony Petrecca <marcanthony.petrecca@gmail.com>
2022-03-15 14:46:00 -0400

    Complete finishing touches

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -5,10 +5,11 @@ const AboutMe = (props) => (
     <img className="proPic" src={props.propic}/>
     <div className="aboutMeTextContainer">
       <h1 className="aboutMeTitle"><strong className="aboutMeTitleStrong">About Me</strong></h1>
-      <p className="aboutMeText">My name is <strong className="name">MarcAnthony Petrecca</strong>. I'm a software engineer located in the greater New York City area. I am a lifelong learner with the drive to use my knowledge to positively impact the world around me. In my free time, I love to follow sports, read, and spend time with friends.</p>
+      <p className="aboutMeText">My name is <strong className="name">MarcAnthony Petrecca</strong>. I'm a software engineer located in the greater New York City area. I am a lifelong learner with the drive to positively impact the world around me. In my free time, I love to follow sports, read, and spend time with friends.</p>
     </div>
   </section>
 );
 
+//to use my knowledge
 
 export default AboutMe;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 import AboutMe from './components/AboutMe';
-import PersonalPhoto from './newphoto.png';
+import PersonalPhoto from './websitephoto.png';
 import WorkAccordian from './components/WorkAccordian';
 import Technology from './components/Technology';
 import ApiImage from './BalancePhoto.png';
@@ -13,13 +13,13 @@ import Footer from './components/Footer';
 const App = () => {
 
   const work = [
-    {title: 'Balance: Exercise and Meal Application', img: 'https://github.com/Wide-Open-Blue-Ocean/Blue-Ocean/raw/main/dist/images/Workout/Workout-All-Open.png', description: "MERN stack desktop application where users create personalized daily regimens for food and exercise, as well as track kpis and personal goals", technology: ["Node.js", "React", "Express", "MongoDB"], github: "https://github.com/Wide-Open-Blue-Ocean/Blue-Ocean", text: ['Worked on a team of 7 developers, each constructing CRUD operations for our respective features.', 'Designed MongoDB schemas and queries for Journal widget, accounting for data integrity and increased read and write capabilities.', 'Employed React to craft front end structure/functionality and Sass to apply typography,  icons, shadows, and other design rules to emphasize  website personality.', 'Implemented agile workflow, addressed time-boxed tickets, and minimized git merge conflicts.', 'Wrote clean, reusable, and secure code through modular programing']},
+    {title: 'Balance: Exercise and Meal Application', img: 'https://github.com/Wide-Open-Blue-Ocean/Blue-Ocean/raw/main/dist/images/Workout/Workout-All-Open.png', description: "MERN stack desktop application where users create personalized daily regimens for food and exercise, as well as track kpis and personal goals", technology: ["Node.js", "React", "Express", "MongoDB"], github: "https://github.com/Wide-Open-Blue-Ocean/Blue-Ocean", text: ['Worked on a team of 7 developers, each constructing CRUD operations for our respective features.', 'Designed MongoDB schemas and queries for Journal widget, accounting for data integrity and increased read and write capabilities.', 'Employed React to craft front end structure/functionality and Sass to apply typography,  icons, shadows, and other design rules to emphasize  website personality.', 'Implemented an agile approach to development, adressing time-boxed tickets and making decisions based on client feedback', 'Administered modular programing to keep code clean, maintainable, and reusable.']},
     {title: 'Swift Clothing Line: E-commerce API', img: `${ApiImage}`, description: "RESTful Question and Answers API for retail E-commerce Website.", technology: ["Node.js", "FS Module", "Express", "MySQL"], github: "https://github.com/Cavatappi-Engineering-Solutions/project-catwalk", text: ['Designed a backend system with schema for MySQL database, crafted RESTful API endpoints, and implemented efficient queries to access data from multiple tables.', 'Performed an extract and transform process on 3 csv files containing millions of records using Node\'s file system, stream, and readline modules to account for in-file memory limitations, helping to decrease load time by 5-6 hours.', 'Optimized queries for performance bottlenecks through indexing and use of complex subqueries, decreasing latency by 99.4%.']},
-    {title: 'Swift Clothing Line: Front End E-commerce Site', img: 'https://github.com/Cavatappi-Engineering-Solutions/project-catwalk/raw/main/client/src/assets/overview/project_catwalk.gif', description: "React Front End application for existing E-commerce Back End.", technology: ["Node.js", "React", "Axios", "Swift API" ], github: "https://github.com/Cavatappi-Engineering-Solutions/project-catwalk", text: ['Constructed front end functionality and design of Questions and Answers module for retail-ecommerce website.', 'Used React, Axios, and JS promises to give users the ability to view and search questions, ask questions, answer questions, and provide feedback on questions about the current product.', 'Implemented Redux store for centralized state, avoiding prop drilling to child components and total page reloads on state updates, leading to a decrease in database requests and increase in website capability.']}
+    {title: 'Swift Clothing Line: Front End E-commerce Site', img: 'https://github.com/Cavatappi-Engineering-Solutions/project-catwalk/raw/main/client/src/assets/overview/project_catwalk.gif', description: "React Front End application for existing E-commerce Back End.", technology: ["Node.js", "React", "Axios", "Swift API" ], github: "https://github.com/Cavatappi-Engineering-Solutions/project-catwalk", text: ['Constructed front end functionality and design of Questions and Answers module for retail-ecommerce website.', 'Used React, Axios, and JS promises to give users the ability to view and search questions, ask questions, answer questions, and provide feedback on questions about the current product.', 'Implemented Redux store for centralized state, eliminating prop drilling, decreasing database requests, and increasing website capability.']}
   ];
 
   const tech = {
-    frontend: ["Javascript (ES5 and ES6)", "React", "Redux", "AJAX", "Axios", "HTML5", "CSS3", "jQuery"],
+    frontend: ["Javascript (ES5 and ES6)", "React", "Redux", "AJAX", "Axios", "HTML5", "CSS3", "Sass", "jQuery"],
     backend: ["RESTful API Development", "Node.js", "Express", "MySQL", "MongoDB", "ETL"],
     testing: ["Mocha / Chai", "Babel", "Webpack", "Git", "Trello"],
     deployment: ["Docker", "AWS (EC2)"]
@@ -75,7 +75,7 @@ const App = () => {
 
         if (href !== '#' && href.startsWith("#")) {
           const section = document.querySelector(href);
-          section.scrollIntoView({behavior: "smooth"});
+          section.scrollIntoView({block: 'start', behavior: "smooth"});
         }
 
       });
